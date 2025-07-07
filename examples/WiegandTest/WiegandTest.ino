10f97662213e46b607343bdc051c75342d3f4b11
codejedi365 <codejedi365@gmail.com>
2025-07-07 11:44:07 -0600

    refactor(example): update example Arduino sketch to match new O-O design

--- examples/WiegandTest/WiegandTest.ino
+++ examples/WiegandTest/WiegandTest.ino
@@ -1,19 +1,63 @@
 #include <Wiegand.h>
 
 // Update these pin definitions if you are using different pins for Wiegand DATA0 and DATA1.
-#define PIN_DATA0  WIEGAND_DEFAULT_PIN_D0
-#define PIN_DATA1  WIEGAND_DEFAULT_PIN_D1
+#define READER_1_PIN_DATA0  WIEGAND_DEFAULT_PIN_D0
+#define READER_1_PIN_DATA1  WIEGAND_DEFAULT_PIN_D1
 
 // Set the baud rate for serial communication
 // Need a high baud rate to avoid data loss because of the high frequency of Wiegand data
 #define BAUD_RATE  115200
 
-#define LAST_SCANNED_CODE_LIFETIME_MS  WIEGAND_CODE_LIFETIME_MS
+
+// Function prototypes
+void check4WiegandData(Wiegand* wiegandReader);
+String formatWiegandData(unsigned long cardCode, int wiegandType);
+void reader1_data0_isr_callback();
+void reader1_data1_isr_callback();
+
 
 // Globals
-static Wiegand wiegandReader1;
-static unsigned long wiegandReader1LastCode = 0;
-static unsigned long wiegandReader1LastCodeTimeMS = 0;
+static Wiegand wiegandReader1 = Wiegand(
+    READER_1_PIN_DATA0, READER_1_PIN_DATA1, reader1_data0_isr_callback, reader1_data1_isr_callback
+);
+
+
+void setup() {
+    Serial.begin(BAUD_RATE);
+
+    // Wait for serial to be ready
+    while (!Serial);
+    Serial.println("Wiegand Reader Ready for User Input");
+
+    // Initialize the Wiegand reader with your defined pins
+    if (!wiegandReader1.begin()) {
+        String err = (
+            "Failed to start interrupt input Wiegand Reader 1 using pins "
+            + String(wiegandReader1.getPinDATA0()) + " and " + String(wiegandReader1.getPinDATA1()));
+        Serial.println(err);
+        return;
+    }
+}
+
+
+void loop() {
+    check4WiegandData(&wiegandReader1);
+
+    // Small delay to prevent busy-waiting and allow other non-interrupt tasks to run
+    delay(1);
+}
+
+
+void check4WiegandData(Wiegand* wiegandReader) {
+    if (wiegandReader->available()) {
+        unsigned long cardCode = wiegandReader->getCode();
+        wiegandReader->clearCodeState();
+
+        // Print the formatted Wiegand data to the serial monitor
+        Serial.println(formatWiegandData(cardCode, wiegandReader->getWiegandType()));
+    }
+}
+
 
 String formatWiegandData(unsigned long cardCode, int wiegandType) {
     // Format the Wiegand data into a human-readable string
@@ -33,41 +77,7 @@ String formatWiegandData(unsigned long cardCode, int wiegandType) {
     return formattedData;
 }
 
-void checkWiegandData(Wiegand* wiegandReader, unsigned long* cachedCode, unsigned long* lastTimestamp) {
-    if (wiegandReader->available()) {
-        unsigned long cardCode = wiegandReader->getCode();
-        unsigned long currentTime = millis();
-        if (cardCode == *cachedCode) {
-            unsigned long elapsedTime = currentTime - *lastTimestamp;
-            if (elapsedTime > LAST_SCANNED_CODE_LIFETIME_MS) {
-                *cachedCode = 0;
-                *lastTimestamp = 0;
-            }
-            return;
-        }
 
-        // Cache the timestamp of the last code so we can only keep it for a limited lifetime
-        *lastTimestamp = currentTime;
+void reader1_data0_isr_callback() { Wiegand::readDATA0(&wiegandReader1); }
 
-        // Cache the last card so it doesn't repeat the print for the lifetime of the wiegand internal cache
-        *cachedCode = cardCode;
-
-        // Print the formatted Wiegand data to the serial monitor
-        Serial.println(formatWiegandData(cardCode, wiegandReader->getWiegandType()));
-    }
-}
-
-void setup() {
-    Serial.begin(BAUD_RATE);
-
-    // Wait for serial to be ready
-    while (!Serial);
-    Serial.println("Wiegand Reader Ready for User Input");
-
-    // Initialize the Wiegand reader with your defined pins
-    wiegandReader1.begin(PIN_DATA0, PIN_DATA1);
-}
-
-void loop() {
-    checkWiegandData(&wiegandReader1, &wiegandReader1LastCode, &wiegandReader1LastCodeTimeMS);
-}
+void reader1_data1_isr_callback() { Wiegand::readDATA1(&wiegandReader1); }
